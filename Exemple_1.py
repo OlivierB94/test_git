67028d7c35d22d001fde32edd43fc44de5214086
OlivierB94 <spt.olivier.94+mooc@gmail.com>
2017-09-20 16:45:19 +0200

    Exemple_1.py - Modif - code final

--- Exemple_1.py
+++ Exemple_1.py
@@ -1,13 +1,24 @@
+import random
+
 quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."] 
 characters = ["Alvin", "Chipmunks", "Babar", "Betty Boop", "Calimero", "Casper", "Le Chat", "Kirikou"]
 
-def show_random_item(my_list):
-    # get a random number
-    item = my_list[0] # get a quote from a list
+def get_random_item(object_list):
+    rand_numb = random.randint(0, len(object_list) - 1)
+    item = object_list[rand_numb] # get a quote from a list
     return item # return the item
 
-user_answer = "A"
+def capitalize(words):
+    for word in words: 
+        return word.capitalize()
+
+def message(character, quote):
+    capitalize(character)
+    capitalize(quote)
+    return "{} a dit : {}".format(character, quote)
+
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
 
 while user_answer != "B":
-    print(show_random_item(quotes))
-	user_answer = "B"
+    print(message(get_random_item(characters), get_random_item(quotes)))
+user_answer = input('Tapez entrée pour connaître une autre citation ou B pour quitter le programme.')
