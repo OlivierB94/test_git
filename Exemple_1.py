c75d455c29a9c4c42910ac0dcb28cd02066ee331
OlivierB94 <spt.olivier.94+mooc@gmail.com>
2017-09-20 16:43:06 +0200

    Exemple_1.py - Modif - sortie bouvle While

--- Exemple_1.py
+++ Exemple_1.py
@@ -1,4 +1,4 @@
-quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."]
+quotes = ["Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", "On doit pouvoir choisir entre s'écouter parler et se faire entendre."] 
 characters = ["Alvin", "Chipmunks", "Babar", "Betty Boop", "Calimero", "Casper", "Le Chat", "Kirikou"]
 
 def show_random_item(my_list):
@@ -10,3 +10,4 @@ user_answer = "A"
 
 while user_answer != "B":
     print(show_random_item(quotes))
+	user_answer = "B"
